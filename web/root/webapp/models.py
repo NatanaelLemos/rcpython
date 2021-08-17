7c54612d7a05fa9c317ca9abd7bbc3dbdf5115cb
Natanael Lemos <natanael.lemos@freshconsulting.com>
2021-08-17 14:43:14 -0700

    post directions from index page

--- web/root/webapp/models.py
+++ web/root/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Command(models.Model):
+    class Direction(models.IntegerChoices):
+        UNKNOWN = 0,
+        UP = 1000,
+        UPLEFT = 1010,
+        UPRIGHT = 1001,
+        RIGHT = 1,
+        DOWNRIGHT = 101,
+        DOWN = 100,
+        DOWNLEFT = 110,
+        LEFT = 10
+
+    direction = models.IntegerField(
+        choices=Direction.choices,
+        default=Direction.UNKNOWN
+    )
--- web/root/webapp/views.py
+++ web/root/webapp/views.py
@@ -1,6 +1,22 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from webapp.models import Command
+
+import logging
+import json
 
 
+@csrf_exempt
 def index(request):
-    return HttpResponse("Hello world!")
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            direction = Command.Direction(data["direction"])
+            logging.warning(direction)
+            return JsonResponse({}, status=200)
+        except:
+            logging.warning("boom!!!")
+            return JsonResponse({}, status=500)
+
+    return render(request, 'index.html')
